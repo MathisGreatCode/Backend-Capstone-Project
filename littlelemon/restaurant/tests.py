96801a7e98c57ea8d6362c0cf273d1e4f94785f7
MathisGreatCode <mathisvarcha@yahoo.fr>
2023-03-16 16:46:44 +0100

    tests added

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 # Create your tests here.
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = MenuItem.objects.create(title="IceCream", price=8, inventory=100)
+        self.assertEqual(str(item), "IceCream : 8")
+
+#class MenuViewTest(TestCase):
+#    def setUp(self):
+#        self.items = [MenuItem.objects.create(title="Chocolate Cake", price=7, inventory=100),
+#                 MenuItem.objects.create(title="Cookies", price=3, inventory=200),
+#                 MenuItem.objects.create(title="pancakes", price=8, inventory=100) ]
+#
+  #  def test_getall(self):
+   #     serialized_data = [MenuItemSerializer(item) for item in self.items]
+    #    self.assertEqual(serialized_data, MenuItemView(self.items???request?) )
